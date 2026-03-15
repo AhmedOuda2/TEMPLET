932754f06aea966dd8828793cc9b1fcec3888c4d
Ahmed Ouda <ahmedouda@fci.bu.edu.eg>
2026-03-15 20:00:14 +0200

    Remove sorting of divisors
    Remove sorting of divisors before returning the vector.

---
+++ Divisors.cpp
@@ -0,0 +1,10 @@
+vector<int> div(int n) {
+    vector<int> v;
+    for (int i = 1; i <= n / i; i++) {
+        if (n % i == 0) {
+            v.push_back(i);
+            v.push_back(n / i);
+        }
+    }
+    return v;
+}
