14bf8a0bd0e77c6242fa28ec9ac88f2aadccc459
Ahmed Ouda <ahmedouda@fci.bu.edu.eg>
2026-08-07 15:51:35 +0300

    Update Divisors.cpp

--- Divisors.cpp
+++ Divisors.cpp
@@ -1,9 +1,9 @@
 vector<int> div(int n) {
     vector<int> v;
-    for (int i = 1; i <= n / i; i++) {
+    for (int i = 1; i * i <= n; i++) {
         if (n % i == 0) {
             v.push_back(i);
-            v.push_back(n / i);
+            if (i != n / i) v.push_back(n / i);
         }
     }
     sort(all(v));
